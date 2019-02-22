17315efca8fc22208f8b0ff603a45a50e748fa82
imstupid1000 <47298624+imstupid1000@users.noreply.github.com>
2019-02-22 11:26:55 -0700

    Update serverinfo.js

--- commands/tools/serverinfo.js
+++ commands/tools/serverinfo.js
@@ -28,23 +28,23 @@ constructor(client)
   .setDescription("This is the main body of text, it can hold 2048 characters.")
   .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
   .setImage("http://i.imgur.com/yVpymuV.png")
-  .setThumbnail("http://i.imgur.com/p2qNFag.png")
+  .setThumbnail(message.guild.iconURL)
   /*
    * Takes a Date object, defaults to current date.
    */
   .setTimestamp()
-  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
-  .addField("This is a field title, it can hold 256 characters",
-    "This is a field value, it can hold 1024 characters.")
+
+  .addField("Verification level",
+    message.guild.verificationLevel)
   /*
    * Inline fields may not display as inline if the thumbnail and/or image is too big.
    */
-  .addField("Inline Field", "They can also be inline.", true)
+  .addField("Member count", message.guild.memberCount)
+        .addField('')
   /*
    * Blank field, useful to create some space.
    */
-  .addBlankField(true)
-  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
+  
  
   message.channel.send({embed});
     }
