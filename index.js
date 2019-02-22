550fabb9d18b9d46b81288d141b19eb2bc2ddefc
imstupid1000 <47298624+imstupid1000@users.noreply.github.com>
2019-02-22 16:12:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -46,11 +46,9 @@ bot.on('ready', function(){
                 .setFooter("Message by " + message.member.displayName, message.member.displayAvatarURL)
                 .setColor('#00FF00')
                            channel.send(embed)
-          
+          })
              
-            }
-    
-            )}
+  
      
 bot.on('guildMemberAdd', function(member){
     if(!member.guild.channels.find('name', 'welcome'))
