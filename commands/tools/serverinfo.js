f1132c5e7bbbec91ac3cf60d2b88e6423c7aa37b
imstupid1000 <47298624+imstupid1000@users.noreply.github.com>
2019-02-22 11:35:40 -0700

    Update serverinfo.js

--- commands/tools/serverinfo.js
+++ commands/tools/serverinfo.js
@@ -25,7 +25,7 @@ constructor(client)
    * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
    */
   .setColor(0x00AE86)
-  .setDescription("This is the main body of text, it can hold 2048 characters.")
+  .setDescription("Self explanatory.")
   .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
   .setImage("http://i.imgur.com/yVpymuV.png")
   .setThumbnail(message.guild.iconURL)
@@ -40,6 +40,7 @@ constructor(client)
    * Inline fields may not display as inline if the thumbnail and/or image is too big.
    */
   .addField("Member count", message.guild.memberCount)
+        .addField('Custom emojis', '(just putting text here to prevent errors)' + message.guild.emojis)
         
   /*
    * Blank field, useful to create some space.
