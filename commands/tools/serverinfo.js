7f7a812a5ae30fcda7701f4c2395262ba3952cbd
imstupid1000 <47298624+imstupid1000@users.noreply.github.com>
2019-02-22 10:09:54 -0700

    Create serverinfo.js

---
+++ commands/tools/serverinfo.js
@@ -0,0 +1,51 @@
+const commando = require('discord.js-commando')
+const Discord = require('discord.js')
+const bot = new commando.Client({
+    owner: '452666956353503252',
+    commandPrefix: '-'
+});
+
+class KickMemberCommand extends commando.Command
+{
+constructor(client)
+    {
+        super(client,{
+            name: 'kick',
+            group: 'tools',
+            memberName: 'kick',
+            description: 'Kicks specified member'
+        });
+    }
+    async run(message, args)
+    {
+        message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Member count",
+        value: "Members in server: " + message.guild.memberCount
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    
+  }
+});
+    }
+  
+
+}
+    module.exports = KickMemberCommand
