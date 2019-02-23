85f82af2b1489dfeeb349a0f8d463b0eb49bb53d
imstupid1000 <47298624+imstupid1000@users.noreply.github.com>
2019-02-23 16:24:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -103,22 +103,30 @@ bot.on('guildMemberAdd', function (member) {
 });
 
 
-    bot.on('guildMemberAdd', async member => {
-        const channel = member.guild.channels.find(ch => ch.name === 'logs');
-        if (!channel) return;
-    
-        const canvas = Canvas.createCanvas(700, 250);
-        const ctx = canvas.getContext('2d');
-    
-        // Since the image takes time to load, you should await it
-        const background = await Canvas.loadImage('./images/wallpaper.jpg');
-        // This uses the canvas dimensions to stretch the image onto the entire canvas
-        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-        // Use helpful Attachment class structure to process the file for you
-        const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
-    
-        channel.send(`Welcome to the server, ${member}!`, attachment);
-    });
+client.on('guildMemberAdd', async member => {
+	const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+	if (!channel) return;
+
+	const canvas = Canvas.createCanvas(700, 250);
+	const ctx = canvas.getContext('2d');
+
+	const background = await Canvas.loadImage('./wallpaper.jpg');
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+	ctx.strokeStyle = '#74037b';
+	ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+	// Get the icon in the form of a buffer
+	const { body: buffer } = await snekfetch.get(member.user.displayAvatarURL);
+	// Wait for Canvas to load the image
+	const avatar = await Canvas.loadImage(buffer);
+	// Draw a shape onto the main canvas
+	ctx.drawImage(avatar, 25, 0, 200, canvas.height);
+
+	const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
+
+	channel.send(`Welcome to the server, ${member}!`, attachment);
+});
 
 
 bot.on('guildCreate', function () {
