f41be35fe647cb04cb66b4461f699951b1704ffd
imstupid1000 <47298624+imstupid1000@users.noreply.github.com>
2019-02-23 15:49:04 -0700

    lol

---
+++ commands/tools/write.js
@@ -0,0 +1,20 @@
+const commando = require('discord.js-commando')
+const bot = new commando.Client({
+    owner: '452666956353503252',
+    commandPrefix: '-'
+});
+
+class KickMemberCommand extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'save-text',
+            group: 'tools',
+            memberName: 'save-text',
+            description: 'Save text to a json file'
+        });
+    }
+    async run(message, args) {
+
+    }
+}
+module.exports = KickMemberCommand
