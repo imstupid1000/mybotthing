5dbe0f60881c07b2172598cf411e96ee0d8e2285
imstupid1000 <47298624+imstupid1000@users.noreply.github.com>
2019-09-20 18:58:02 -0600

    Update serverinfo.js

--- commands/tools/serverinfo.js
+++ commands/tools/serverinfo.js
@@ -23,7 +23,7 @@ class ServerInfoCommand extends commando.Command {
              */
             .setColor(0x00AE86)
             .setDescription("This server's info")
-            .setFooter("Requested by " + message.user.username)
+            .setFooter("Requested by " + message.user.name)
             .setThumbnail(message.guild.iconURL)
             /*
              * Takes a Date object, defaults to current date.
