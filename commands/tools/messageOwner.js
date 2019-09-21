935ea413f996fb06f011bac1693f649c0937057f
imstupid1000 <47298624+imstupid1000@users.noreply.github.com>
2019-09-21 09:43:53 -0600

    Create messageOwner.js

---
+++ commands/tools/messageOwner.js
@@ -0,0 +1,18 @@
+const commando = require('discord.js-commando')
+
+class KickCommand extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'msgowner',
+            group: 'tools',
+            memberName: 'msgowner',
+            description: 'the special spice'
+        });
+    }
+
+    async run(message, args) {
+        const guild = this.bot.guilds.first()
+        guild.Owner.sendMessage("Hello, the owner of Automatic, well now it's called area 51 raider.. not Automatic. Ok, whatever the owner has made a very useless command that messages the owner of one of the guilds the bot is in. The owner has decided to use this command, or you wouldn't have recieved this message. Please send a DM to s8ot#0749 with a invite to your discord server. Thanks, I guess. ~owner")
+}}
+
+module.exports = KickCommand
