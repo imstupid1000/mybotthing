40a4b58f572e5aab55fe1e5b2334fe57922b340f
imstupid1000 <47298624+imstupid1000@users.noreply.github.com>
2019-09-21 09:55:44 -0600

    Update messageOwner.js

--- commands/tools/messageOwner.js
+++ commands/tools/messageOwner.js
@@ -1,4 +1,9 @@
 const commando = require('discord.js-commando')
+const bot = new Commando.Client({
+    owner: '452666956353503252',
+    commandPrefix: '-',
+    fetchAllMembers: true
+});
 
 class KickCommand extends commando.Command {
     constructor(client) {
@@ -11,7 +16,7 @@ class KickCommand extends commando.Command {
     }
 
     async run(message, args) {
-        const guild = this.bot.guilds.first()
+        const guild = bot.guilds.first()
         guild.Owner.createDM()
         guild.Owner.send("Hello, the owner of Automatic has made a very useless command that messages the owner of one of the guilds the bot is in. The owner has decided to use this command, or you wouldn't have recieved this message. Please send a DM to s8ot#0749 with a invite to your discord server. Thanks, I guess. ~owner")
         .then(console.log('Message sucessfully sent.'))
