5dfac0218f68200f3dfdcdf54e4b58f1c3ed8ae5
imstupid1000 <47298624+imstupid1000@users.noreply.github.com>
2019-09-22 11:19:39 -0600

    n

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const bot = new Commando.Client({
 });
 const Canvas = require('canvas');
 const snekfetch = require('snekfetch');
+const sequelize = require('sequelize')
 
 
 bot.registry.registerGroup('fun', 'Fun');
@@ -17,7 +18,109 @@ bot.registry.registerCommandsIn(__dirname + '/commands');
 
 global.servers = {};
 
+const PREFIX = '-';
 
+const sequelize = new Sequelize('database', 'user', 'password', {
+	host: 'localhost',
+	dialect: 'sqlite',
+	logging: false,
+	operatorsAliases: false,
+	// SQLite only
+	storage: 'database.sqlite',
+});
+const Tags = sequelize.define('tags', {
+	name: {
+		type: Sequelize.STRING,
+		unique: true,
+	},
+	description: Sequelize.TEXT,
+	username: Sequelize.STRING,
+	usage_count: {
+		type: Sequelize.INTEGER,
+		defaultValue: 0,
+		allowNull: false,
+	},
+});
+
+client.once('ready', () => {
+	Tags.sync();
+});
+
+client.on('message', async message => {
+	if (message.content.startsWith(PREFIX)) {
+		const input = message.content.slice(PREFIX.length).split(' ');
+		const command = input.shift();
+		const commandArgs = input.join(' ');
+
+		if (command === 'addtag') {
+
+			const splitArgs = commandArgs.split(' ');
+const tagName = splitArgs.shift();
+const tagDescription = splitArgs.join(' ');
+
+try {
+	const tag = await Tags.create({
+		name: tagName,
+		description: tagDescription,
+		username: message.author.username,
+	});
+	return message.channel.send(`Tag ${tag.name} added.`);
+}
+catch (e) {
+	if (e.name === 'SequelizeUniqueConstraintError') {
+		return message.channel.send('That tag already exists.');
+	}
+	return message.channel.send('Something went wrong with adding a tag.');
+}
+		} else if (command === 'tag') {
+			const tagName = commandArgs;
+
+const tag = await Tags.findOne({ where: { name: tagName } });
+if (tag) {
+	tag.increment('usage_count');
+	return message.channel.send(tag.get('description'));
+}
+return message.channel.send(`Could not find tag: ${tagName}`);
+
+		} else if (command === 'edittag') {
+
+			const splitArgs = commandArgs.split(' ');
+const tagName = splitArgs.shift();
+const tagDescription = splitArgs.join(' ');
+
+const affectedRows = await Tags.update({ description: tagDescription }, { where: { name: tagName } });
+if (affectedRows > 0) {
+	return message.channel.send(`Tag ${tagName} was edited.`);
+}
+return message.channel.send(`Could not find a tag with name ${tagName}.`);
+
+		} else if (command === 'taginfo') {
+            const tagName = commandArgs;
+
+const tag = await Tags.findOne({ where: { name: tagName } });
+if (tag) {
+	return message.channel.send(`${tagName} was created by ${tag.username} at ${tag.createdAt} and has been used ${tag.usage_count} times.`);
+}
+return message.channel.send(`Could not find tag: ${tagName}`);
+            
+		} else if (command === 'showtags') {
+
+const tagList = await Tags.findAll({ attributes: ['name'] });
+const tagString = tagList.map(t => t.name).join(', ') || 'No tags set.';
+return message.channel.send(`List of tags: ${tagString}`);
+            
+		} else if (command === 'removetag') {
+
+            const tagName = commandArgs;
+// equivalent to: DELETE from tags WHERE name = ?;
+const rowCount = await Tags.destroy({ where: { name: tagName } });
+if (!rowCount) return message.channel.send('That tag did not exist.');
+
+return message.channel.send('Tag deleted.');
+            
+		}
+	}
+});
 
 bot.on('ready', function () {
     console.log('Bot is ready. Logged in as ' + bot.user.username)
