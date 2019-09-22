44f463981ad68f7fafa8c383da9dfe1c12c756d9
imstupid1000 <47298624+imstupid1000@users.noreply.github.com>
2019-09-22 12:08:58 -0600

    Create leave.js

---
+++ commands/music/leave.js
@@ -0,0 +1,27 @@
+const Commando = require('discord.js-commando')
+const Discord = require('discord.js')
+
+class LeaveChannelCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'leavechannel',
+            group: 'music',
+            memberName: 'leavechannel',
+            description: 'Leaves current voice channel.'
+        });
+    }
+
+    async run(message, args) {
+        if(message.guild.voiceConnection) {
+            message.guild.voiceConnection.disconnect()
+            .then(connection => {
+                message.channel.send('Sucessfully left voice channel.')
+            });
+        }
+        else {
+            message.channel.send('I am not in a voice channel.')
+        }
+    }
+}
+
+module.exports = LeaveChannelCommand
