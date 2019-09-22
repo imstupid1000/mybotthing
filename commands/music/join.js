c1ed584aa3de566aab034b5deba3939af3ef7e08
imstupid1000 <47298624+imstupid1000@users.noreply.github.com>
2019-09-22 11:59:05 -0600

    Create join.js

---
+++ commands/music/join.js
@@ -0,0 +1,30 @@
+const Commando = require('discord.js-commando')
+const Discord = require('discord.js')
+
+class JoinChannelCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'joinchannel',
+            group: 'music',
+            memberName: 'joinchannel',
+            description: 'Joins your voice channel.'
+        });
+    }
+
+    async run(message, args) {
+        if(message.member.voiceChannel) {
+            if(message.guild.voiceConnection) {
+                message.member.voiceChannel.join()
+                .then(connection => {
+                    message.channel.send('Successfully joined channel.')
+                });
+            }
+            else {
+                message.channel.send('You must be in a voice channel for me to join it.s')
+            }
+        }
+
+    }
+}
+
+module.exports = JoinChannelCommand
