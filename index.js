568d3eb98eea37eca32e968cd398465451f266d4
imstupid1000 <47298624+imstupid1000@users.noreply.github.com>
2020-09-05 20:16:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const bot = new Commando.Client({
     commandPrefix: '-',
     fetchAllMembers: true
 });
-
 const snekfetch = require('snekfetch');
 const Sequelize = require('sequelize')
 
@@ -70,77 +69,6 @@ bot.on('messageDelete', function (message) {
 })
 
 
-
-
-bot.on('guildMemberAdd', function (member) {
-    if (!member.guild.roles.find("name", 'Member')) {
-        member.guild.createRole({
-            name: 'Member',
-            color: '00FF00'
-        }).then(function () {
-            member.send('Welcome to ' + member.guild.name + '!')
-            let memberRole = member.guild.roles.find("name", 'Member');
-            member.addRole(memberRole);
-        })
-    } else {
-        member.send('Welcome to ' + member.guild.name + '!')
-        let memberRole = member.guild.roles.find("name", 'Member');
-        member.addRole(memberRole);
-    }
-});
-
-
-const applyText = (canvas, text) => {
-    const ctx = canvas.getContext('2d');
-    let fontSize = 70;
-
-    do {
-        ctx.font = `${fontSize -= 10}px helvetica`;
-    } while (ctx.measureText(text).width > canvas.width - 300);
-
-    return ctx.font;
-};
-
-
-bot.on('guildMemberAdd', async member => {
-    const channel = member.guild.channels.find(ch => ch.name === 'logs');
-    if (!channel) return;
-
-    const canvas = Canvas.createCanvas(700, 250);
-    const ctx = canvas.getContext('2d');
-    const welcomes = require('./welcomes.json')
-    const values = Object.values(welcomes)
-    const welcomemessage = values[parseInt(Math.random() * values.length)]
-
-    const background = await Canvas.loadImage('./images/wallpaper.jpg');
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-    ctx.strokeStyle = '#74037b';
-    ctx.strokeRect(0, 0, canvas.width, canvas.height);
-    ctx.font = '28px arial';
-    ctx.fillStyle = '#ffffff';
-    ctx.fillText(welcomemessage, canvas.width / 2.5, canvas.height / 3.5);
-
-
-    ctx.font = applyText(canvas, `${member.displayName}!`);
-    ctx.fillStyle = '#ffffff';
-    ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
-
-    ctx.beginPath();
-    ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-    ctx.closePath();
-    ctx.clip();
-
-    const {
-        body: buffer
-    } = await snekfetch.get(member.user.displayAvatarURL);
-    const avatar = await Canvas.loadImage(buffer);
-    ctx.drawImage(avatar, 25, 25, 200, 200);
-
-    const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
-
-    channel.send(`${welcomemessage}, ${member}!`, attachment);
-});
 
 
 bot.on('guildCreate', function () {
